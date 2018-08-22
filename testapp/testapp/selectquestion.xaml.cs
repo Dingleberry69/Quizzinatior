a384a35a2604ed37ec13f6d4e4c1d412bc12aae1
Dingleberry69 <36864062+Dingleberry69@users.noreply.github.com>
2018-08-22 14:48:21 -0600

    minor changes

--- testapp/testapp/selectquestion.xaml.cs
+++ testapp/testapp/selectquestion.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace testapp
 {
@@ -37,22 +26,28 @@ namespace testapp
             {
                 if((string)quizzes.SelectedItem == key)
                 {
-                    q.qtext.Text = key;
-                    q.A1text.Text = questions[key][0];
-                    q.A2text.Text = questions[key][1];
-                    q.A3text.Text = questions[key][2];
-                    q.A4text.Text = questions[key][3];
+                    q.qtext.Text = key.Trim();
+                    questionedit.oq = key.Trim();
+                    q.A1text.Text = questions[key][0].Trim();
+                    questionedit.o1 = questions[key][0].Trim();
+                    q.A2text.Text = questions[key][1].Trim();
+                    questionedit.o2 = questions[key][1].Trim();
+                    q.A3text.Text = questions[key][2].Trim();
+                    questionedit.o3 = questions[key][2].Trim();
+                    q.A4text.Text = questions[key][3].Trim();
+                    questionedit.o4 = questions[key][3].Trim();
+                    questionedit.ori = questions[key][4].Trim();
                     questions.Remove(key);
                     break;
                 }
             }
-
+            questionedit.questions = questions;
             back.Content = q;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            back.Content = new editquiz();
         }
     }
 }
